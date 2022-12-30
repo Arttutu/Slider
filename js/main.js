18019ffee3599add35afc565071dc221949dccc5
Arthur <arthurtt08@gmail.com>
2022-12-30 11:27:16 -0300

    finalizando -js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ $(document).ready(function () {
   let textoCamila =
     "Sou a psicóloga Camila Andrade e  tenho como objetivo prestar uma escuta especializada, em que o paciente possa fazer uso da psicoterapia como um lugar transformador. Sou pós graduanda em Terapia ABA e TCC. Minha atuação é dentro da abordagem cognitiva-comportamental, realizando a modalidade de psicoterapia breve, os mais diversos públicos"
 
+  let textoAna =
+    "“Bacharel em Administração de empresas pelo Centro Universitário Fiam- Faam, possuo facilidade em trabalhar em equipe, proatividade e foco são qualidades que me acompanham. Considero-me uma excelente profissional que estará agregando valor a sua equipe."
+
+  let textoGil =
+    "Cozinheira proativa com domínio de diversas gastronomias e mais de 5 anos de experiência no ramo, atuo como uma souschef informal no meu emprego atual. Além da prática, tenho vasto conhecimento teórico adquirido na faculdade de gastronomia. Adoraria uma oportunidade de um entrevista, na qual discutirei como poderia utilizar meus talentos como chefe de partida do seu hotel.r"
+
   $("#anterior").click(function () {
     if (contador == 0) {
       contador = -1
@@ -32,19 +38,45 @@ $(document).ready(function () {
   $("#proximo").click(function () {
     if (contador2 == 0) {
       contador2 = 1
-      $("p").replaceWith(
-        "<p>Sou a psicóloga Camila Andrade e  tenho como objetivo prestar uma escuta especializada, em que o paciente possa fazer uso da psicoterapia como um lugar transformador. Sou pós graduanda em Terapia ABA e TCC. Minha atuação é dentro da abordagem cognitiva-comportamental, realizando a modalidade de psicoterapia breve, os mais diversos públicos</p>"
-      )
+      $("p").replaceWith("<p>" + textoCamila + "</p>")
+      $("#nome").replaceWith("<span id='nome'> Camila Andrade </span>")
+      $("#profissao").replaceWith("<span id='profissao'>Psicóloga </span>")
     } else if (contador2 == 1) {
       contador2 = -1
-      $("p").replaceWith(
-        "<p>Ainda não sei o que vou colocar aqui, mas você que está lendo esse texto sinta se a vontade para me ajudar</p>"
+      $("p").replaceWith("<p>" + textoGil + "</p>")
+      $("#nome").replaceWith("<span id='nome'> Gil Andrade </span>")
+      $("#profissao").replaceWith(
+        "<span id='profissao'>Chef de Cozinha </span>"
       )
     } else if (contador2 == -1) {
-      $("P").replaceWith(
-        "<p>“Bacharel em Administração de empresas pelo Centro Universitário Fiam- Faam, possuo facilidade em trabalhar em equipe, proatividade e foco são qualidades que me acompanham. Considero-me uma excelente profissional que estará agregando valor a sua equipe. teste“</p>"
+      $("P").replaceWith("<p>" + textoAna + "</p>")
+      $("#nome").replaceWith("<span id='nome'> Ana Clara </span>")
+      $("#profissao").replaceWith(
+        "<span id='profissao'>Analista Backoffice </span>"
       )
       contador2 = 0
+    }
+  })
+  $("#anterior").click(function () {
+    if (contador2 == 0) {
+      contador2 = -1
+      $("p").replaceWith("<p>" + textoGil + "</p>")
+      $("#nome").replaceWith("<span id='nome'> Gil Andrade </span>")
+      $("#profissao").replaceWith(
+        "<span id='profissao'>Chef de Cozinha </span>"
+      )
+    } else if (contador2 == -1) {
+      contador2 = 1
+      $("p").replaceWith("<p> " + textoCamila + "</p>")
+      $("#nome").replaceWith("<span id='nome'> Camila Andrade </span>")
+      $("#profissao").replaceWith("<span id='profissao'>Psicóloga </span>")
+    } else if (contador2 == 1) {
+      contador2 = 0
+      $("P").replaceWith("<p>" + textoAna + "</p>")
+      $("#nome").replaceWith("<span id='nome'> Ana Clara </span>")
+      $("#profissao").replaceWith(
+        "<span id='profissao'>Analista Backoffice </span>"
+      )
     }
   })
 })
